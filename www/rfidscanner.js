0ae64bbeec8d1d56527c4a0ddf75527342acb4a4
Luiz Ávila <lapavila@gmail.com>
2013-11-04 23:46:44 -0200

    Fix javascript funcion

--- www/rfidscanner.js
+++ www/rfidscanner.js
@@ -1,31 +1,6 @@
-cordova.define("cordova/plugin/rfidscanner", function(require, exports, module) {
-
-    var exec = require('cordova/exec');
-
-    function RfidScanner() {
-
-    };
-
-    RfidScanner.prototype.scan = function (successCallback, errorCallback) {
-        if (errorCallback == null) {
-            errorCallback = function () {
-            };
-        }
-
-        if (typeof errorCallback != "function") {
-            console.log("RfidScanner.scan failure: failure parameter not a function");
-            return;
-        }
-
-        if (typeof successCallback != "function") {
-            console.log("RfidScanner.scan failure: success callback parameter must be a function");
-            return;
-        }
-
-        exec(successCallback, errorCallback, 'RfidScanner', 'scan', []);
-    };
-
-    var rfidScanner = new RfidScanner();
-    module.exports = rfidScanner;
-
-});
+var rfidscanner = {
+    scan: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', []);
+    }
+}
+module.exports = rfidscanner;
