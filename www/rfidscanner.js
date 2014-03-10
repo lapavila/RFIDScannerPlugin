90a7a925950ca01dba54b40efea36c00df4f2207
Luiz Ávila <lapavila@gmail.com>
2014-03-10 15:02:40 -0300

    add javascript radar

--- www/rfidscanner.js
+++ www/rfidscanner.js
@@ -2,5 +2,8 @@ var rfidscanner = {
     scan: function(successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', []);
     }
+    radar: function(successCallback, errorCallback, tags) {
+         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', tags);
+    }
 }
 module.exports = rfidscanner;
