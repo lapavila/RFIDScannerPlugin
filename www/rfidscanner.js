b41d3db6a783a77b8e5cf072f2b40e9b745f3d50
Luiz Ávila <lapavila@gmail.com>
2014-03-10 15:04:50 -0300

    fix error

--- www/rfidscanner.js
+++ www/rfidscanner.js
@@ -3,7 +3,7 @@ var rfidscanner = {
         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', []);
     }
     radar: function(successCallback, errorCallback, tags) {
-         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', tags);
+         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'radar', tags);
     }
 }
 module.exports = rfidscanner;
