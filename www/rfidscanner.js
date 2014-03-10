f5251c1bedceb4032e24f10c2c32e6e5bc2eb737
Luiz Ávila <lapavila@gmail.com>
2014-03-10 17:25:18 -0300

    fix

--- www/rfidscanner.js
+++ www/rfidscanner.js
@@ -3,8 +3,6 @@ var rfidscanner = {
         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'scan', []);
     },
     radar: function(successCallback, errorCallback, tags) {
-    	alert(tags);
-    	alert(tags.lenght);
         cordova.exec(successCallback, errorCallback, 'RfidScanner', 'radar', tags);
     }
 };
